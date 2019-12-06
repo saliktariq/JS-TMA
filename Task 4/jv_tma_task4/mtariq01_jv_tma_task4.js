5e9a9be002b7ac8f6d9a9cfdb7ac9f05203e8b11
saliktariq <saliktariq@icloud.com>
2019-12-06 01:47:31 +0000

    Update mtariq01_jv_tma_task4.js

--- Task 4/jv_tma_task4/mtariq01_jv_tma_task4.js
+++ Task 4/jv_tma_task4/mtariq01_jv_tma_task4.js
@@ -5,7 +5,13 @@
 **  @author Salik Tariq
 **/
 
-
+let lakeData = {};
+lakeData.assign = {name: "Caspian Sea", depth: 560};
+lakeData.assign = {name: "Tarn Hows", depth: 53};
+lakeData.assign = {name: "Crater Lake", depth: 324};
+lakeData.assign = {name: "Lake Tanganyika", depth: 803};
+lakeData.assign = {name: "Lake Vostok", depth: 546};
+lakeData.assign = {name: "Lake Baikal", depth: 897};
 
 
  
