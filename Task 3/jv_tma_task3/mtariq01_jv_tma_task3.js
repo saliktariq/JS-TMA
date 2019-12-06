91704ec2e08798e4f30be5273f93dcb64d9a5c82
saliktariq <saliktariq@icloud.com>
2019-12-06 01:02:26 +0000

    Final commit Task 3

---
+++ Task 3/jv_tma_task3/mtariq01_jv_tma_task3.js
@@ -0,0 +1,52 @@
+/**
+**  An application that calculates the ranking of a senior chess
+**  player based on his/her ranking points
+**  @author Salik Tariq
+**/
+
+/**
+** Function to determine the ranking of a player based on points. 
+** takes as an argument rankingPoints of type Number.
+** @return a string containing ranking of the player.
+**/ 
+
+function calculateRank(rankingPoints){
+
+	switch (true){
+		case (rankingPoints >= 1600 && rankingPoints <= 1799):
+			return "Class B";
+			break;
+		
+		case (rankingPoints >= 1800 && rankingPoints <= 1999):
+			return "Class A";
+			break;
+			
+		case (rankingPoints >= 2000 && rankingPoints <= 2199):
+			return "Expert";
+			break;
+		
+		case (rankingPoints >= 2200 && rankingPoints <= 2399):
+			return "National master";
+			break;
+		
+		case (rankingPoints >= 2400):
+			return "Senior master";
+			break;
+		
+	}
+
+}
+
+/**
+** Function to display the ranking of a player based on points. 
+** takes as an argument rankingPoints of type Number.
+** @return a string containing ranking of the player output to console.
+**/ 
+
+function displayRanking(rankingPoints){
+	console.log(calculateRank(rankingPoints));
+}
+
+//** Function call for the example output **//
+
+displayRanking(2400);
