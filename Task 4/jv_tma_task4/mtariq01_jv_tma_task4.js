6841469f9281c356aa44ceb909a3544c45d48f42
saliktariq <saliktariq@icloud.com>
2019-12-06 01:12:11 +0000

    Update mtariq01_jv_tma_task4.js

---
+++ Task 4/jv_tma_task4/mtariq01_jv_tma_task4.js
@@ -0,0 +1,11 @@
+/**
+**  An application to create a nested data structure representing lake depths
+**  in fathoms. It finds the deepest lake in the collection, and
+**  display its name and depth in metres. 
+**  @author Salik Tariq
+**/
+
+
+
+
+ 
