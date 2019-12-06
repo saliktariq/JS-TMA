bf65697d66e744acd6ad2b20dc5720205792d415
saliktariq <saliktariq@icloud.com>
2019-12-06 00:16:32 +0000

    Update mtariq01_jv_tma_task2.js

--- Task 2/jv_tma_task2/mtariq01_jv_tma_task2.js
+++ Task 2/jv_tma_task2/mtariq01_jv_tma_task2.js
@@ -4,3 +4,35 @@
 **  @author Salik Tariq
 **/
 
+/**
+** Function to calculate the fuel efficiency in metric units. 
+** takes as an argument kilometers of type Number and litres of type Number.
+** @return a string containing fuel efficiency in metric units.
+**/ 
+
+function fuelEfficiencyMetric(kilometers, litres){
+	return "Your car has a fuel economy of " + ((litres/kilometers)*100).toFixed(2) + " litres per hundred kilometers.";
+}
+
+/**
+** Function to calculate the fuel efficiency in imperial units. 
+** takes as an argument kilometers of type Number and litres of type Number.
+** @return a string containing fuel efficiency in imperial units.
+**/ 
+ 
+function fuelEfficiencyImperial(kilometers, litres){
+	/** @param miles and gallons of type Number to contain converted values 
+	    Coversion value source(s): 
+		https://www.metric-conversions.org/volume/uk-gallons-to-liters.htm
+        https://www.metric-conversions.org/length/kilometers-to-miles.htm
+	**/
+	let miles = kilometers * 0.62137;
+	let gallons = litres * 0.219969;
+	
+	return "Your car has a fuel economy of " + ((gallons/miles)*100).toFixed(2) + " gallons per hundred miles.";	
+}
+
+// Code to output result on the console.
+
+console.log(fuelEfficiencyMetric(500,52.28));
+console.log(fuelEfficiencyImperial(500,52.28));
